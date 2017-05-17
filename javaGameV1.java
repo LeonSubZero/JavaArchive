6fb37c35a9582c86bf9931e789a9391859108992
LeonSubZero <leon.flavio@gmail.com>
2017-05-17 13:39:58 -0400

    Add files via upload

---
+++ javaGameV1.java
@@ -0,0 +1,38 @@
+/*  Flavio Leon
+ * Sept 14 2016
+ * JavaGame1
+ */
+
+		import java.util.Scanner; //needed to do console input
+
+		public class inputoutput {
+			//every program has a main method
+			public static void main(String[] args) {
+				/*Declare and initialize variables and constants
+				int numShirts = 0, numPants = 0, numShorts = 0;
+				double totalCost = 0.0, totalTax = 0.0, shirtSub = 0.0, pantsSub = 0.0, shortsSub = 0.0;
+				final double SHIRT_COST = 22.5, PANTS_COST = 29.5, SHORTS_COST = 14.0;
+				final double SALES_TAX_RATE = 0.06;
+				*/
+				
+				//create a Scanner object for input
+				Scanner userInput = new Scanner(System.in);
+				
+				//Intro
+				System.out.println("WELCOME TO THE JAVA JUNGLE GAME!!\n");
+				
+				//prompt for name
+				userInput = JOptionPane.showInputDialog("Enter your name: ");
+				name = Integer.parseInt(userInput); //converts to an int
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
